69928c2bb0c12a576a5179419c4149f3002dd759
Troy Grosfield <troy.grosfield@gmail.com>
2013-12-20 07:35:42 -0700

    Pep8 fixes and settings name fix.

--- django_user_connections/__init__.py
+++ django_user_connections/__init__.py
@@ -11,18 +11,21 @@ def get_user_connection_model():
     from django.conf import settings
     from django.db.models import get_model
 
-    if not hasattr(settings, 'NOTIFICATION_MODEL'):
+    if not hasattr(settings, 'USER_CONNECTION_MODEL'):
         from .models import UserConnection
         return UserConnection
 
     try:
         app_label, model_name = settings.USER_CONNECTION_MODEL.split('.')
     except ValueError:
-        raise ImproperlyConfigured("USER_CONNECTION_MODEL must be of the form 'app_label.model_name'")
+        raise ImproperlyConfigured("USER_CONNECTION_MODEL must be of the form "
+                                   "'app_label.model_name'")
 
     user_connection_model = get_model(app_label, model_name)
 
     if user_connection_model is None:
-        raise ImproperlyConfigured("USER_CONNECTION_MODEL refers to model '%s' that has not been installed" % settings.USER_CONNECTION_MODEL)
+        raise ImproperlyConfigured("USER_CONNECTION_MODEL refers to model '%s' "
+                                   "that has not been installed" %
+                                   settings.USER_CONNECTION_MODEL)
 
     return user_connection_model
