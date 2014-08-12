2c49d99a5653e31ef0dec958ce9b3a0b5de6f147
Troy Grosfield <troy.grosfield@gmail.com>
2014-08-12 13:45:58 -0600

    removed django-nose test args.

--- tests/settings.py
+++ tests/settings.py
@@ -1,8 +1,6 @@
 from __future__ import unicode_literals
 
 import os
-import sys
-
 
 USER_CONNECTION_MODEL_MIXIN = 'tests.test_models.AbstractUserConnectionMixin'
 USER_CONNECTION_MANAGER = 'tests.test_models.managers.UserConnectionManager'
@@ -50,6 +48,3 @@ DATABASES = {
         'NAME': here('test_db.db')
     }
 }
-
-if 'test' in sys.argv:
-    NOSE_ARGS = ('--nocapture', '--with-doctest', '--testmatch=^test')
